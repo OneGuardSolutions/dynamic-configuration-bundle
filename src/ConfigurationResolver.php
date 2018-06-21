0eac08e3fd58b8d8306b3d19179ca8d929559bfc
Tomáš Pecsérke <tomas.pecserke@oneguard.email>
2018-06-21 16:01:22 +0200

    added copyright notice to all files
    Signed-off-by: Tomáš Pecsérke <tomas.pecserke@oneguard.email>

--- src/ConfigurationResolver.php
+++ src/ConfigurationResolver.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the OneGuard DynamicConfigurationBundle.
+ *
+ * (c) OneGuard <contact@oneguard.email>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace OneGuard\Bundle\DynamicConfigurationBundle;
 
 use OneGuard\Bundle\DynamicConfigurationBundle\Entity\ConfigurationValue;
